f36757e411febe02cf4c1087ed2f92ec325d752e
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-15 11:09:11 -0800

    adds more packages and a server with GET end point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-console.log("hello deeksha")
-console.log("hello harit")
-console.log("hello kumi")
+import express from 'express';
+import debug from 'debug';
+
+const app = express();
+const log: debug.IDebugger = debug('app:debugger');
+
+app.get('/', (req, res) => {
+    res.send("Hello there, how are you?")
+})
+
+app.listen(3400,() =>  log('Your application is running on port 3400'))
+
