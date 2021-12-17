686d7e907f1a02b2ddbc2372f7571af1b6248740
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-17 12:52:11 -0800

    testing husky

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const log: debug.IDebugger = debug('http:debugger');
 
 app.get('/', (req, res) => {
-  console.log('dgjw,wejlfkwekjehe');
+  console.log('dgjw,wejlfkwqwdwefeekjehe');
   res.send('Hello there, how are you?');
 });
 
