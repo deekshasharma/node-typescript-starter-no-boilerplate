af6ee2232934a4d601e168b7ad174951a9ad53e4
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-17 13:05:45 -0800

    staging files in src directory

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const log: debug.IDebugger = debug('http:debugger');
 
 app.get('/', (req, res) => {
   // console.log('dgjw,  wedwewejlfkwqwdwefeekjehe');
-        res.send('Hello there, how are you?')
+  res.send('Hello there, how are you?');
 });
 
 app.listen(3400, () => log('Your application is running on port 3400'));
