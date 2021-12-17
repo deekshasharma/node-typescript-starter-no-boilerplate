2eb96b61cfcf87bf7bd7cd4e1fc087e8f071d416
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-17 13:08:55 -0800

    staging files in src directory

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 const log: debug.IDebugger = debug('http:debugger');
 
 app.get('/', (req, res) => {
-  // console.log('dgjw,  wedwewejlfkwqwdwefeekjehe');
   res.send('Hello there, how are you?');
 });
 
