6a07a1e4241d5b3bd163279a0fa8e00be235bd60
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-17 12:58:35 -0800

    remove staged lint

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const app = express();
 const log: debug.IDebugger = debug('http:debugger');
 
 app.get('/', (req, res) => {
-  console.log('dgjw,  wedwewejlfkwqwdwefeekjehe');
-  res.send('Hello there, how are you?');
+  // console.log('dgjw,  wedwewejlfkwqwdwefeekjehe');
+        res.send('Hello there, how are you?')
 });
 
 app.listen(3400, () => log('Your application is running on port 3400'));
