a579a75e71816e4342534b050a22f7f890ed52a4
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-17 08:02:04 -0800

    test commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 const log: debug.IDebugger = debug('http:debugger');
 
 app.get('/', (req, res) => {
+    console.log('Hello world!');
+
+    for (let i = 0; i < 12; i++) {
+        console.log(i);
+    }
     res.send("Hello there, how are you?")
 })
 
